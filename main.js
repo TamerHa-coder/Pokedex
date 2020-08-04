bb10be40e7d069318742192fc7e77924705aff9a
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-08-04 19:09:57 +0300

    Show list of types names for each pockemon

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const pokemonDiv = document.getElementById("pokemonDiv");
 const searchPokemon = async (pokemonId) => { 
   try{
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  makeDiv(data.name, data.height, data.weight,data.sprites.front_default, data.sprites.back_default);
+  //const { types } = await axios.get(`https://pokeapi.co/api/v2/type/${pokemonId}`);
+  makeDiv(data.name, data.height, data.weight,data.sprites.front_default, data.sprites.back_default, data.types);
+  console.log(data.types);
   searchInput.value="";
   searchInput.focus();
 } catch(err){
@@ -15,21 +17,33 @@ const searchPokemon = async (pokemonId) => {
 }
 };
 
-const makeDiv = (name, height, weight, picture, pictureBack) => {
+const makeDiv = (name, height, weight, picture, pictureBack, types) => {
   let nameDiv = document.createElement("div");
   let heightDiv = document.createElement("div");
   let weightDiv = document.createElement("div");
   let pictureDiv = document.createElement("img");
+  let typesDiv = document.createElement("div");
   pictureDiv.src=picture;
 
   nameDiv.innerHTML ="Name: " + name;
   heightDiv.innerHTML = "Height: " + height;
   weightDiv.innerHTML = "Weight: " + weight;
+  typesDiv.innerHTML = "Type: ";
+
+  for (let i=0; i< types.length; i++){
+    let typeName = types[i].type.name;
+    let typeUrl = types[i].type.url;
+    let liElement = document.createElement('li');
+    typesDiv.appendChild(liElement);
+    const htmlTextTypes = `<a href= "#" , onclick='sameTypePokemons(${typeUrl})'> ${typeName} </a>`;
+    liElement.innerHTML= htmlTextTypes;
+  }
 
   pokemonDiv.appendChild(nameDiv);
   pokemonDiv.appendChild(heightDiv);
   pokemonDiv.appendChild(weightDiv);
   pokemonDiv.appendChild(pictureDiv);
+  pokemonDiv.appendChild(typesDiv);
 
   pictureDiv.onmouseover=()=>pictureDiv.src=pictureBack;
   pictureDiv.onmouseout=()=>pictureDiv.src=picture;
